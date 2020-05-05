bca11adf364220ed2e2aec4dcc9a60f269e82957
Marcin Hercog <mkp.hercog@gmail.com>
2020-05-05 20:24:10 +0200

    Updates

---
+++ precache-manifest.45a89dad27816e978198c46128068a63.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75be41eef39a4b79bb4411ad2f77ebcd",
+    "url": "/space/index.html"
+  },
+  {
+    "revision": "389d7c54ee6d2ca01012",
+    "url": "/space/static/css/main.1de3abd2.chunk.css"
+  },
+  {
+    "revision": "20f29e22f3b26aefc957",
+    "url": "/space/static/js/2.6d0e4700.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/space/static/js/2.6d0e4700.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "389d7c54ee6d2ca01012",
+    "url": "/space/static/js/main.a56e49e1.chunk.js"
+  },
+  {
+    "revision": "1e28d00aa17cd92ba1ba",
+    "url": "/space/static/js/runtime-main.041acb0b.js"
+  },
+  {
+    "revision": "2c7ebd72adb8bcaee18fa78348780587",
+    "url": "/space/static/media/space-bg.2c7ebd72.jpg"
+  },
+  {
+    "revision": "b22799e8b85d90b4598956fb7eb8efe1",
+    "url": "/space/static/media/space-bg2.b22799e8.jpg"
+  }
+]);
