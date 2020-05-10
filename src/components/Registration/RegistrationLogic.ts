388e72464844f13335721eb7cc6c40a0b3bbe3c6
Marcin Hercog <mkp.hercog@gmail.com>
2020-05-10 17:55:40 +0200

    add space secure in password and login

--- src/components/Registration/RegistrationLogic.ts
+++ src/components/Registration/RegistrationLogic.ts
@@ -25,8 +25,11 @@ export const validation = (
   name: string,
   password: string
 ): boolean | string => {
-  const regExp = new RegExp("[0-9]", "g");
-  const passwordWithTwoNumbers = regExp.test(password);
+  const isNumberRegExp = new RegExp("[0-9]", "g");
+  const isSpaceRegExp = new RegExp(" ", "g");
+  const spaceInLogin = isSpaceRegExp.test(login);
+  const passwordWithNumbers = isNumberRegExp.test(password);
+  const spaceInPassword = isSpaceRegExp.test(password);
   const minLoginAndNameLength = 3;
   const maxLoginAndNameLength = 20;
   const minPasswordLength = 5;
@@ -37,13 +40,17 @@ export const validation = (
     return `Login musi mieć co najmniej ${minLoginAndNameLength} znaki.`;
   } else if (login.length > maxLoginAndNameLength) {
     return `Login jest za długi (max ${maxLoginAndNameLength} znaków).`;
+  } else if (spaceInLogin) {
+    return `Login nie może posiadać spacji.`;
   } else if (name.length < minLoginAndNameLength) {
     return `Nazwa musi mieć co najmniej ${minLoginAndNameLength} znaki.`;
   } else if (name.length > maxLoginAndNameLength) {
     return `Nazwa jest za długa (max ${maxLoginAndNameLength} znaków).`;
   } else if (password.length < minPasswordLength) {
     return `Hasło musi mieć co najmniej ${minPasswordLength} znaków.`;
-  } else if (!passwordWithTwoNumbers) {
+  } else if (!passwordWithNumbers) {
     return "Hasło musi zawierać jedną lub więcej cyfr.";
+  } else if (spaceInPassword) {
+    return "Hasło nie może posiadać spacji.";
   } else return true;
 };
