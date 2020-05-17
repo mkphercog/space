0dc73c38f9c4892d0a62921b3c903394624a596a
Marcin Hercog <mkp.hercog@gmail.com>
2020-05-17 19:05:58 +0200

    add searching input in users list change admin .gif to .jpg

--- src/components/AllUsersList/AllUsersList.tsx
+++ src/components/AllUsersList/AllUsersList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AllUsersList.scss";
 import { useDispatch } from "react-redux";
 import { UserToAdd } from "./UserToAdd/UserToAdd";
@@ -7,12 +7,18 @@ export const AllUsersList: React.FC<UsersProps> = ({
   loggedUser,
   allUsersList,
 }) => {
+  const [searchValue, setSearchValue] = useState("");
   const dispatch = useDispatch();
+
   const getListWithoutLoggedUser = allUsersList.filter(
     (user) => user.id !== loggedUser.id
   );
 
-  const renderUsersList = getListWithoutLoggedUser.map((globalUser) => {
+  const filterUsersList = getListWithoutLoggedUser.filter((globalUser) =>
+    globalUser.name.toUpperCase().includes(searchValue.toUpperCase())
+  );
+
+  const renderUsersList = filterUsersList.map((globalUser) => {
     const isInLoggedUserList = loggedUser.friends.includes(globalUser.id);
     return (
       <UserToAdd
@@ -24,7 +30,22 @@ export const AllUsersList: React.FC<UsersProps> = ({
     );
   });
 
-  return <section className="all-users-list">{renderUsersList}</section>;
+  return (
+    <section className="all-users-list">
+      <input
+        className="all-users-list__search"
+        type="text"
+        placeholder="Wyszukaj..."
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+      />
+      {renderUsersList.length ? (
+        renderUsersList
+      ) : (
+        <p className="all-users-list__no-result">Brak wyników</p>
+      )}
+    </section>
+  );
 };
 
 interface UsersProps {
